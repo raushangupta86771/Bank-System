b7fdf38711b573b40454e474045e1b310a0079ce
raushangupta86771 <raushandsa@gmail.com>
2023-04-01 23:56:53 +0530

    modified the oops part of profile

--- index.js
+++ index.js
@@ -13,8 +13,18 @@ mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology
   .then(() => console.log('Connected to MongoDB database'))
   .catch(error => console.error('Could not connect to MongoDB database', error));
 
-// Define User schema
-const userSchema = new mongoose.Schema({
+// Define User class
+class User {
+  constructor(username, password, pin) {
+    this.username = username;
+    this.password = password;
+    this.pin = pin;
+    this.wallet = 0;
+    this.transactions = [];
+  }
+}
+
+User.schema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   pin: { type: String, required: true },
@@ -22,20 +32,26 @@ const userSchema = new mongoose.Schema({
   transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Transaction' }]
 });
 
+User.model = mongoose.model('User', User.schema);
 
-// Define Transaction schema
-const transactionSchema = new mongoose.Schema({
+// Define Transaction class
+class Transaction {
+  constructor(sender, receiver, amount) {
+    this.sender = sender;
+    this.receiver = receiver;
+    this.amount = amount;
+    this.date = Date.now();
+  }
+}
+
+Transaction.schema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   amount: { type: Number, required: true },
   date: { type: Date, default: Date.now }
 });
 
-// Define User model
-const User = mongoose.model('User', userSchema);
-
-// Define Transaction model
-const Transaction = mongoose.model('Transaction', transactionSchema);
+Transaction.model = mongoose.model('Transaction', Transaction.schema);
 
 // Middleware
 app.use(express.json());
@@ -58,17 +74,11 @@ const auth = (req, res, next) => {
 
 // Signup route
 app.post('/signup', async (req, res) => {
-  // Check if there are any validation errors
-  // const errors = validationResult(req);
-  // if (!errors.isEmpty()) {
-  //   return res.status(400).json({ errors: errors.array() });
-  // }
-
   const { username, password, pin } = req.body;
 
   try {
     // Check if user with the given email already exists
-    let user = await User.findOne({ username });
+    let user = await User.model.findOne({ username });
     if (user) {
       return res.status(400).json({ msg: 'User already exists' });
     }
@@ -78,11 +88,7 @@ app.post('/signup', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, salt);
 
     // Create a new user with the provided information
-    user = new User({
-      username,
-      password: hashedPassword,
-      pin
-    });
+    user = new User(username, hashedPassword, pin);
 
     // Save the user to the database
     await user.save();
@@ -94,131 +100,85 @@ app.post('/signup', async (req, res) => {
   }
 });
 
-
 // Login route
 app.post('/login', async (req, res) => {
-  // Check if there are any validation errors
-
   const { username, password, pin } = req.body;
 
   try {
     // Check if user with the given email exists
-    let user = await User.findOne({ username });
+    let user = await User.model.findOne({ username });
     if (!user) {
       return res.status(400).json({ msg: 'Invalid credentials' });
     }
 
-    // Check if password is correct
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
+    // Check if the password and pin match the user's stored password and pin
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    const isPinMatch = await bcrypt.compare(pin, user.pin);
+    if (!isPasswordMatch || !isPinMatch) {
       return res.status(400).json({ msg: 'Invalid credentials' });
     }
-
-    // Check if pin is correct
-    if (pin !== user.pin) {
-      return res.status(400).json({ msg: 'Invalid credentials' });
-    }
-
-    // Generate and sign a JWT token that includes the user ID and pin
-    const payload = {
-      user: {
-        id: user.id,
-        pin: user.pin
-      }
-    };
-
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '1h'
-    });
+    // Create and sign a JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
     res.json({ token });
+
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
 });
 
-
-// Add money route
-app.post('/add-money', auth, async (req, res) => {
+// Get current user's profile route
+app.get('/profile', auth, async (req, res) => {
   try {
-    const { amount, pin, username } = req.body;
-    const user = await User.findOne({username});
-    if (!user) return res.status(404).json({ message: 'User not found' });
-
-    // Check if the provided pin matches the user's pin
-    const isPinValid = await bcrypt.compare(pin, user.pin);
-    if (!isPinValid) {
-      return res.status(401).json({ message: 'Invalid pin' });
-    }
-
-    // Add money to the user's wallet
-    user.wallet += amount;
-    await user.save();
-    res.json({ message: 'Money added successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Could not add money' });
+    // Find the current user's profile using the userId obtained from the auth middleware
+    const user = await User.model.findById(req.userId).populate('transactions', '-_id -__v');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
 });
 
+// Transfer money route
+app.post('/transfer', auth, async (req, res) => {
+  const { receiverUsername, amount } = req.body;
 
-// Pay money route
-app.post('/pay-money', auth, async (req, res) => {
   try {
-    const { username, amount } = req.body;
-    const sender = await User.findById(req.userId);
-    const receiver = await User.findOne({ username });
-    if (!sender || !receiver) return res.status(404).json({ message: 'User not found' });
-    if (sender.wallet < amount) return res.status(400).json({ message: 'Insufficient balance' });
+    // Find the current user and the receiver using their usernames
+    const sender = await User.model.findById(req.userId);
+    const receiver = await User.model.findOne({ username: receiverUsername });
+
+    // Check if the receiver exists
+    if (!receiver) {
+      return res.status(400).json({ msg: 'Receiver does not exist' });
+    }
+
+    // Check if the sender has enough money in their wallet
+    if (sender.wallet < amount) {
+      return res.status(400).json({ msg: 'Insufficient balance' });
+    }
+
+    // Deduct the amount from the sender's wallet and add it to the receiver's wallet
     sender.wallet -= amount;
     receiver.wallet += amount;
-    await sender.save();
-    await receiver.save();
-    const transaction = new Transaction({ sender: sender._id, receiver: receiver._id, amount });
+
+    // Create a new transaction and add it to both the sender's and receiver's transactions arrays
+    const transaction = new Transaction(sender._id, receiver._id, amount);
     await transaction.save();
     sender.transactions.push(transaction);
     receiver.transactions.push(transaction);
+
+    // Save the sender and receiver to the database
     await sender.save();
     await receiver.save();
-    res.json({ message: 'Money paid successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Could not pay money' });
-  }
-});
 
-// Add the auth middleware to the /profile route
-app.get('/profile', auth, async (req, res) => {
-  try {
-    // Find the user by ID and populate the 'transactions' field
-    const user = await User.findById(req.userId).populate('transactions');
-    console.log(req.userId)
-
-    // If user is not found, return a 404 response
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    // Return the user's profile information
-    res.json({ username: user.username, wallet: user.wallet, transactions: user.transactions });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Could not get profile' });
-  }
-});
-
-
-// Admin route
-app.get('/admin', auth, async (req, res) => {
-  try {
-    const users = await User.find().select('username wallet transactions').populate('transactions');
-    res.json(users);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Could not get users' });
+    res.json({ msg: 'Transaction successful' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
 });
 
 // Start server
-app.listen(5000, () => console.log('Server started on port 5000'));
+app.listen(process.env.PORT, () => console.log("Server running on port ${ process.env.PORT }"));
